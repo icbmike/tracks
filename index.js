c2db3353783f6dd13163ec5a7cc05bc972d1e6b8
Michael Little <bio595@gmail.com>
2015-02-15 21:34:52 +1100

    something

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-
-function playTone(source, frequency, time){
-	source.frequency.value = frequency; // value in hertz
-	source.start();
-	source.stop(time || 0);
-}
+var q = require('q');
 
 var audioContext = new AudioContext();
 
-var source = audioContext.createOscillator();
-source.type = 'square';
+
+function playTone(frequency, time){
+	var source = audioContext.createOscillator();
+	source.type = 'square';
+	source.frequency.value = frequency; // value in hertz
+	source.connect(audioContext.destination);
+	source.start();
+	
+	var deferred = q.defer();
+	setTimeout(function(){
+		source.stop();
+		deferred.resolve();
+	}, time || 0);
+
+	return deferred.promise;
+}
 
 
-source.connect(audioContext.destination);
-
-playTone(source, 220, 1);
-playTone(source, 440, 1);
-playTone(source, 220, 1);
-playTone(source, 440, 1);
+playTone(220, 1000, '1')
+.then(function(){
+	return playTone(440, 1000);
+})
+.then(function(){
+	return playTone(220, 1000);
+})
+.then(function(){
+	return playTone(440, 1000);
+});

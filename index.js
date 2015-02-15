8a96fc8376a56fe7a742203c1781a47b68436d32
Michael Little <bio595@gmail.com>
2015-02-15 22:45:33 +1100

    Created a react app

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-var q = require('q');
 
-var audioContext = new AudioContext();
+var React = require('react');
+var App = require('./app.jsx');
 
-
-function playTone(frequency, time){
-	var source = audioContext.createOscillator();
-	source.type = 'square';
-	source.frequency.value = frequency; // value in hertz
-	source.connect(audioContext.destination);
-	source.start();
-	
-	var deferred = q.defer();
-	setTimeout(function(){
-		source.stop();
-		deferred.resolve();
-	}, time || 0);
-
-	return deferred.promise;
-}
-
-
-playTone(220, 1000, '1')
-.then(function(){
-	return playTone(440, 1000);
-})
-.then(function(){
-	return playTone(220, 1000);
-})
-.then(function(){
-	return playTone(440, 1000);
-});
+React.render(
+	React.createElement(App),
+	document.body
+);
